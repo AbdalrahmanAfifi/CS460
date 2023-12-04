77da03b091442d07801d222416ff63c53511ea52
Starrie Schulz <mss00029@mix.wvu.edu>
2023-12-03 22:44:20 -0500

    corrected routing

--- CS460/src/app/app.routes.ts
+++ CS460/src/app/app.routes.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TwitterComponent } from "./twitter/twitter.component";
 import {HomeComponent} from "./home/home.component";
+import {RedditComponent} from "./reddit/reddit.component";
 
 export const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'twitter', component: TwitterComponent },
-  { path: 'reddit', component: TwitterComponent },
+  { path: 'reddit', component: RedditComponent },
 ];
 
 
